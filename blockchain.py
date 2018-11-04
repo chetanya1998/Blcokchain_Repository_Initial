2124fc54ba56ce03cda328dea1b12c87db8967bf
chetanya1998 <31440658+chetanya1998@users.noreply.github.com>
2018-11-05 02:11:16 +0530

    Add files via upload
    This is python code for blockchain.
    It can be made or deployed in any language but i prefer python for the purpose of ease and popularity of the language.
    its a simple initial code for creating blockchain  
    Now if we talk about the functionality and working of code here are the list below:-
     1.first function initiates the block function-> it creates the block
     2.second function gets the information of new transaction and changes commit in a block
     3. third function returns the values possess  by last block  of block chain.

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+class Blockchain(object):
+    """docstring fo Blockchain."""
+    def __init__(self, arg):
+        self.chain=[]
+        self.current_transaction=[]
+
+    def new_block(self):
+        #creating new block and adds it to the Blockchain
+        pass
+
+    def new_transaction(self):
+        # adds  a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        #returns the last block in the Blockchain
+        pass
